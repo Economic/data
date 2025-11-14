ac5adc3d50fde4d0c6568658e3746777bcc18460
zmokhiber <zmokhiber@epi.org>
2025-11-13 20:54:38 -0500

    prepare version 2025.11.13

--- _targets.R
+++ _targets.R
@@ -3,7 +3,7 @@ source("./packages.R")
 
 ## Update these for a new doc push and release
 ### Data release version
-data_release_version = "2025.9.15"
+data_release_version = "2025.11.13"
 
 # Dashboard data location
 dashboard_data_path = Sys.getenv("DASHBOARD_DATA_DIR")
