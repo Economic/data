d4a0a680bf5bc65842cbc51ceb60d6a0cbb00d17
zmokhiber <zmokhiber@epi.org>
2026-02-27 17:09:14 -0500

    Release version 2026.2.27: fix union membership smoothing calculation

--- _targets.R
+++ _targets.R
@@ -3,7 +3,7 @@ source("./packages.R")
 
 ## Update these for a new doc push and release
 ### Data release version
-data_release_version = "2026.2.23"
+data_release_version = "2026.2.27"
 
 # Dashboard data location
 dashboard_data_path = Sys.getenv("DASHBOARD_DATA_DIR")
